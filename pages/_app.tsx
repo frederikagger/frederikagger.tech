b65e64619f6e3011b0b5e64b5cddd108875ef5d1
frederikagger <frederikagger@hotmail.com>
2021-06-30 17:53:38 +0200

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import '../styles/tailwind.css'
+import Header from '../components/Header'
+
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  )
 }
 export default MyApp
---
+++ tailwind.config.js
@@ -0,0 +1,75 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      darkred: '#DB2777',
+      darkgrey: '#626262',
+      white: '#ffff'
+    },
+    fontFamily: {
+      sans: [
+        'Oxanium',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"'
+      ]
+    },
+    extend: {
+      width: {
+        '1/20': '5%',
+        '2/20': '10%',
+        '3/20': '15%',
+        '4/20': '20%',
+        '5/20': '25%',
+        '6/20': '30%',
+        '7/20': '35%',
+        '8/20': '40%',
+        '9/20': '45%',
+        '10/20': '50%',
+        '11/20': '55%',
+        '12/20': '60%',
+        '13/20': '65%',
+        '14/20': '70%',
+        '15/20': '75%',
+        '16/20': '80%',
+        '17/20': '85%',
+        '18/20': '90%',
+        '19/20': '95%',
+        '20/20': '100%'
+      },
+      fontFamily: {
+        body: ['Oxanium', 'ui-sans-serif', 'system-ui']
+      },
+      keyframes: {
+        wave: {
+          '0%': {
+            transform: 'rotate(-5deg) translateX(-2px) translateY(-1px)'
+          },
+          '50%': { transform: 'rotate(25deg)' },
+          '100%': {
+            transform: 'rotate(55deg) translateX(2px) translateY(1px)'
+          }
+        }
+      },
+      animation: {
+        wave: 'wave 0.55s linear infinite alternate'
+      }
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
