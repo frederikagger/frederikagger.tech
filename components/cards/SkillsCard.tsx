b90dfbebd51810c7e1d153758ab1e46cbffaa9ac
frederikagger <frederikagger@hotmail.com>
2021-09-18 23:18:18 +0200

    created skillscard

---
+++ components/cards/SkillsCard.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import { Skill } from '../sections/Skills'
+
+const Card: React.FC<{ skill: Skill }> = ({ skill }) => {
+  const [showText, setShowText] = useState(false)
+  return (
+    <div className='shadow-2xl'>
+      <a href={skill.link || ''}>
+        <h3 
+          style={{ backgroundColor: skill.color }}
+          className='text-center text-xl rounded-lg rounded-b-none border-0 text-white opacity-95  px-1.5 py-2 font-body'
+        >
+        
+          {skill?.name}
+        </h3>
+      </a>
+      <p
+        style={{ borderColor: skill.color }}
+        className='border-2 border-t-0 font-extralight leading-loose text-base rounded-t-none rounded-lg p-3.5'
+      >
+        {skill.description ||
+          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum itaque repellat, expedita fuga dicta maiores atque. Officiis aut iure inventore laudantium harum, magni nam corporis quae error ullam quibusdam officia.'}
+      </p>
+    </div>
+  )
+}
+
+export default Card
