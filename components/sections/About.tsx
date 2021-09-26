09e8a7ad5c6811263b471cbeb181467b2da4022b
frederikagger <frederikagger@hotmail.com>
2021-09-26 12:33:39 +0200

    add description and change some styling

--- components/sections/About.tsx
+++ components/sections/About.tsx
@@ -1,16 +1,16 @@
 export default function About() {
   return (
     <section id='about' className='mx-auto px-4 sm:px-6'>
-      <h1 className='mb-3 pt-16 md:pt-24'>ABOUT ME</h1>
+      <h1 className='mb-3 pt-16 md:pt-24'>ABOUT</h1>
       <p className='leading-loose'>
-        {"I'm"} very passionate about technology and webdesign. I like to learn
-        and dream.
+        {"I'm"} a very passionate person who enjoys technology, animals,
+        politics, science and much more.
       </p>
       <p className='leading-loose mt-8'>
         currently employed at{' '}
         <a
           target='_blank'
-          className='font-bold'
+          className='font-bold hover:text-blue'
           href='http://woodsense.dk'
           rel='noreferrer'
         >
@@ -18,10 +18,10 @@ export default function About() {
         </a>{' '}
         where {"I'm"} working as a frontend developer. Woodsense is a green
         startup that is working on making the construction industry greener by
-        reducing waste wood constructions. You can read more about Woodsense{' '}
+        reducing waste in wood constructions. You can read more about Woodsense{' '}
         <a
           target='_blank'
-          className='font-bold'
+          className='font-bold hover:text-blue'
           href='http://woodsense.dk'
           rel='noreferrer'
         >
@@ -32,8 +32,8 @@ export default function About() {
 
       <p className='mt-8 leading-loose'>
         When {"I'm"} not working as a developer, I enjoy taking my dog to the
-        dogpark, drinking coffe around Copenhagen and reading the newspaper on
-        my balcony. {"I'm"} a big dogperson.
+        park or walk around Copenhagen while enjoying a cup of coffee. I like
+        being out in nature and explore.
         <br />
       </p>
     </section>
