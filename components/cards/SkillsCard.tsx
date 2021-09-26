a31701a3d0b46932814fa1e36386eb1f164071b0
frederikagger <frederikagger@hotmail.com>
2021-09-26 12:32:14 +0200

    fix shadow

--- components/cards/SkillsCard.tsx
+++ components/cards/SkillsCard.tsx
@@ -4,19 +4,18 @@ import { Skill } from '../sections/Skills'
 const Card: React.FC<{ skill: Skill }> = ({ skill }) => {
   const [showText, setShowText] = useState(false)
   return (
-    <div className='shadow-2xl'>
+    <div className=''>
       <a href={skill.link || ''}>
-        <h3 
+        <h3
           style={{ backgroundColor: skill.color }}
           className='text-center text-xl rounded-lg rounded-b-none border-0 text-white opacity-95  px-1.5 py-2 font-body'
         >
-        
           {skill?.name}
         </h3>
       </a>
       <p
         style={{ borderColor: skill.color }}
-        className='border-2 border-t-0 font-extralight leading-loose text-base rounded-t-none rounded-lg p-3.5'
+        className='border-2 shadow-2xl border-t-0 font-extralight leading-loose text-base rounded-t-none rounded-lg p-3.5'
       >
         {skill.description ||
           'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum itaque repellat, expedita fuga dicta maiores atque. Officiis aut iure inventore laudantium harum, magni nam corporis quae error ullam quibusdam officia.'}
