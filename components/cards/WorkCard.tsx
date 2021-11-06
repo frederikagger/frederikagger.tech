a1d338cdbe14f5bc37061c58dd22ac8a5704b61b
frederikagger <frederikagger@hotmail.com>
2021-11-06 17:14:30 +0100

    add link to images

--- components/cards/WorkCard.tsx
+++ components/cards/WorkCard.tsx
@@ -4,14 +4,17 @@ import { Project } from '../sections/Work'
 
 const WorkCard: React.FC<{ project: Project }> = ({ project }) => {
   return (
-    <div className='shadow-2xl cursor-pointer'>
-      <h3>{project.name}</h3>
-      <Image
-        height={470}
-        width={960}
-        src={'/images/' + project.img}
-        alt={project.alt}
-      />
+    <div className='shadow-2xl transform-gpu transition-transform hover:scale-105'>
+      <a href={project.url}>
+        <h3>{project.name}</h3>
+        <Image
+          className='rounded-md '
+          height={470}
+          width={960}
+          src={'/images/' + project.img}
+          alt={project.alt}
+        />
+      </a>
     </div>
   )
 }
