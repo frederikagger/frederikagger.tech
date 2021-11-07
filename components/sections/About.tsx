a078c2a22150edfde97d4ec50a8b00fcaf866b42
frederikagger <frederikagger@hotmail.com>
2021-11-07 14:59:44 +0100

    change about section

--- components/sections/About.tsx
+++ components/sections/About.tsx
@@ -1,12 +1,11 @@
-export default function About() {
+const About = () => {
   return (
     <section id='about' className='mx-auto px-4 sm:px-6'>
       <h1 className='mb-3 pt-24'>ABOUT</h1>
       <p className='leading-loose'>
-        I live in Nørrebro in Copenhagen with my girlfriend and our beautiful
-        Yorkie Arnold 👩‍❤️‍👨🐶
+        I live in Nørrebro in Copenhagen with my girlfriend and our Yorkie
+        Arnold 👩‍❤️‍👨🐶
       </p>
-      <br />
       <p>
         I have a lot of interests. The primary being technology, music and
         politics.
@@ -21,26 +20,16 @@ export default function About() {
         >
           Woodsense
         </a>{' '}
-        where {"I'm"} working as a frontend developer. Woodsense is a green
-        startup that is working on making the construction industry greener by
-        reducing waste in wood constructions. You can read more about Woodsense{' '}
-        <a
-          target='_blank'
-          className='font-bold hover:text-blue'
-          href='http://woodsense.dk'
-          rel='noreferrer'
-        >
-          here
-        </a>
-        .
+        where {"I'm"} working as a frontend developer.
       </p>
 
       <p className='mt-8 leading-loose'>
-        When {"I'm"} not working as a developer, I enjoy taking my dog to the
-        park or walk around Copenhagen while enjoying a cup of coffee. I like
-        being out in nature and explore.
+        If you have any questions or just want to get in touch feel free to drop
+        me a message!
         <br />
       </p>
     </section>
   )
 }
+
+export default About
